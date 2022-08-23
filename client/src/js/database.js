612ba37c39aba80ed11d0c5ea6aa8d28f0593f6b
NateB <nathanbrengman@gmail.com>
2022-08-22 20:12:07 -0500

    added code to database.js

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -13,9 +13,25 @@ const initdb = async () =>
   });
 
 // TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => console.error('putDb not implemented');
+export const putDb = async (content) => {
+    const editorDb = await openDB('jate', 1);
+    const transactVar = editorDb.transaction('jate', 'readwrite');
+    const store = transactVar.objectStore('jate');
+    const req = store.put({ id: 1, value: content });
+
+    const res = await req;
+    console.log('Saved data', res.value)
+}
 
 // TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => console.error('getDb not implemented');
+export const getDb = async () => {
+    const editorDb = await openDB('jate', 'readonly');
+    const store = transactVar.objectStore('jate');
+    const req = store.get(1);
+    const res = await req;
+    res
+    ? console.log('Data gathered from db', res.value)
+    : console.log('Data not found')
+}
 
 initdb();
